89284d2fb461b66d37f65235a9d6a5125a8f07a6
yak <umbertoiozzoli@gmail.com>
2026-09-24 19:28:55 +0200

    driver

--- include/driver.hpp
+++ include/driver.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <cstddef>
+
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <ostream>
 #include <string>
@@ -10,9 +11,11 @@
 namespace Driver {
 class Driver {
 private:
-  std::string res;
+  std::string res = "";
+  std::string tmp;
+  std::stringstream buffer;
   std::size_t fileSize = 0;
-  bool outstages = true;
+  bool printStage1 = false;
 
 public:
   Driver(std::string filePath) {
@@ -21,24 +24,25 @@ public:
     std::string nameStage1 = filePath + "Stage1.txt";
     std::ofstream stage(nameStage1);
 
-    try {
-
-      while (std::getline(fileSrc, res)) {
-        std::cout << res << "\n";
-        if (outstages) {
-
-          stage << res;
-        }
-        fileSize = res.size();
-      }
-      std::cout << res << "file size:" << fileSize << "byte";
-    } catch (...) {
+    if (!fileSrc.is_open()) {
       fileSrc.close();
-      std::cerr << "Faild to open \"" + filePath + "\"" << std::endl;
+      std::cerr << "Failed to open \"" + filePath + "\"" << std::endl;
       exit(1);
     }
+
+      while (std::getline(fileSrc,  tmp)) {
+        if (printStage1)std::cout << tmp << "\n";
+        buffer << tmp;
+        stage << tmp;
+      }
+
+    res = buffer.str();
+
     fileSrc.close();
     stage.close();
   }
+
+  std::string getStage1() { return res; }
+  void setPrintStage1(bool print) { printStage1 = print; }
 };
 } // namespace Driver
